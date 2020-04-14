22417514cce96bcb4c7ac3254a8b3eed2ff27d5e
Muhammad Fathi Aflah <63639867+mfathiaflah2@users.noreply.github.com>
2020-04-14 17:30:33 +0700

    home web menghitung bangun luar

---
+++ tugasuts-home.php
@@ -0,0 +1,40 @@
+<?php
+
+<div id="body">
+ 
+<h1>Hitung Bangun Balok</h1>
+ 
+     
+<li><?php echo anchor('hitung/luasbalok','Luas balok'); ?></li>
+ 
+     
+<li><?php echo anchor('hitung/volbalok','Volume balok'); ?></li>
+ 
+ 
+<h1>Hitung Bangun Tabung</h1>
+ 
+     
+<li><?php echo anchor('hitung/luastabung','Luas Tabung'); ?></li>
+ 
+     
+<li><?php echo anchor('hitung/voltabung','Volume Tabung'); ?></li>
+ 
+ 
+<h1>Hitung Bangun Prisma Segitiga</h1>
+ 
+     
+<li><?php echo anchor('hitung/luasprisma','Luas Prisma Segiempat'); ?></li>
+ 
+     
+<li><?php echo anchor('hitung/volprisma','Volume Prisma Segiempat'); ?></li>
+ 
+ 
+<h1>Hitung Bangun Limas Segiempat Sama Sisi</h1>
+ 
+     
+<li><?php echo anchor('hitung/luaslimas','Luas Limas Segiempat Sama Sisi'); ?></li>
+ 
+     
+<li><?php echo anchor('hitung/vollimas','Volume Limas Segiempat Sama Sisi'); ?></li>
+ 
+</div>
